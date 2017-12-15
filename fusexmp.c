c858af91eddb36d2c01376550acd50d44c62248a
KimHyungChan <hj1003hj@ajou.ac.kr>
2017-12-15 23:21:49 +0900

    completed link!!

--- fusexmp.c
+++ fusexmp.c
@@ -66,8 +66,7 @@ static int xmp_getattr(const char *path, struct stat *stbuf)
   driveB_res=lstat(fullpaths[1],&buf[1]);
   
   if (driveA_res == -1 || driveB_res == -1) return -errno;
-
-  buf[0].st_size+=buf[1].st_size;
+  if (S_ISREG(buf[0].st_mode)) buf[0].st_size+=buf[1].st_size;
   *stbuf = buf[0];
   return 0;
 }
@@ -265,18 +264,19 @@ static int xmp_rename(const char *from, const char *to)
 
 static int xmp_link(const char *from, const char *to)
 {
-  char read_fullpath[PATH_MAX];
+  char read_fullpaths[2][PATH_MAX];
   char write_fullpaths[2][PATH_MAX];
   int res;
 
-  sprintf(read_fullpath, "%s%s",
-      rand() % 2 == 0 ? global_context.driveA : global_context.driveB, from);
+  //sprintf(read_fullpath, "%s%s",rand() % 2 == 0 ? global_context.driveA : global_context.driveB, from);
 
+  sprintf(read_fullpaths[0], "%s%s", global_context.driveA, from);
+  sprintf(read_fullpaths[1], "%s%s", global_context.driveB, from);
   sprintf(write_fullpaths[0], "%s%s", global_context.driveA, to);
   sprintf(write_fullpaths[1], "%s%s", global_context.driveB, to);
 
   for (int i = 0; i < 2; ++i) {
-    res = link(read_fullpath, write_fullpaths[i]);
+    res = link(read_fullpaths[i], write_fullpaths[i]);
     if (res == -1)
       return -errno;
   }
